c6249fd868d0029d357492690ce831b07af0bd33
Gregory Thomas <gdenvert13@gmail.com>
2021-09-02 21:51:57 -0400

    initial commit

---
+++ first_tutorial.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"example.com/gthomas/first_tutorial/morestrings"
+	"github.com/google/go-cmp/cmp"
+)
+
+func main() {
+	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
+	fmt.Println(cmp.Diff("Hello World", "Hello Go"))
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module example.com/gthomas/first_tutorial
+
+go 1.17
+
+require github.com/google/go-cmp v0.5.6
